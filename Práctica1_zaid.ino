06380617d287c1b88007fb1a6945cc54e7858f7d
Z41d-vf <146023904+Z41d-vf@users.noreply.github.com>
2023-10-05 12:45:40 -0600

    Create Práctica1_zaid.ino

---
+++ Práctica1_zaid.ino
@@ -0,0 +1,16 @@
+void setup() {  
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+}
+
+void loop() {
+  
+  digitalWrite(LED_BUILTIN, HIGH); 
+  
+  delay(2000);
+  
+  digitalWrite(LED_BUILTIN, LOW);
+  
+  delay(1500);
+  
+}
